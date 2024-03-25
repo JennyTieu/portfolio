d024f3f022e6bfc4b6406275f3d11238fd73986a
JennyTieu <57066145+JennyTieu@users.noreply.github.com>
2024-03-25 14:05:35 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ const deTexts = document.querySelectorAll("[deText]");
 
 x.addEventListener('change', function () {
   for (let i = 0; i < engTexts.length; i++) {
-    if (this.checked) {
+    if (x.checked) {
       console.log("checked");
       deTexts[i].style.display = "none"
       engTexts[i].style.display = "block"
