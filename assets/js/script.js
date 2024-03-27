930c6d3f2cf8edbac6110589e80458a01bbe9c1e
JennyTieu <57066145+JennyTieu@users.noreply.github.com>
2024-03-27 13:42:19 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,42 +109,25 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 // contact form variables
-const engform = document.querySelector("[data-form-eng]");
-const engformInputs = document.querySelectorAll("[data-form-input-eng]");
-const engformBtn = document.querySelector("[data-form-btn-eng]");
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
 
 // add event to all form input field
-for (let i = 0; i < engformInputs.length; i++) {
-  engformInputs[i].addEventListener("input", function () {
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
 
     // check form validation
-    if (engform.checkValidity()) {
-      engformBtn.removeAttribute("disabled");
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
     } else {
-      engformBtn.setAttribute("disabled", "");
+      formBtn.setAttribute("disabled", "");
     }
 
   });
 }
 
-// contact form variables
-const deform = document.querySelector("[data-form-de]");
-const deformInputs = document.querySelectorAll("[data-form-input-de]");
-const deformBtn = document.querySelector("[data-form-btn-de]");
 
-// add event to all form input field
-for (let i = 0; i < deformInputs.length; i++) {
-  deformInputs[i].addEventListener("input", function () {
-
-    // check form validation
-    if (deform.checkValidity()) {
-      deformBtn.removeAttribute("disabled");
-    } else {
-      deformBtn.setAttribute("disabled", "");
-    }
-
-  });
-}
 
 
 
